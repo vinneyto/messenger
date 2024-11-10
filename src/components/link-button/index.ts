3b8d3cef79505c12f6d99cfae9b8443e6cbc2f81
vinneyto <vinneyto@gmail.com>
2024-11-10 17:20:41 +0100

    makes application true single page

--- src/components/link-button/index.ts
+++ src/components/link-button/index.ts
@@ -1,5 +1,21 @@
+import { Router } from '../../Router';
 import { styled } from '../../styled';
 import tpl from './link-button.hbs';
 import cs from './link-button.module.css';
+
+export function initLinkButton(router: Router) {
+  document.addEventListener('click', (e) => {
+    if (
+      e.target instanceof HTMLButtonElement &&
+      e.target.matches('[data-href]')
+    ) {
+      e.preventDefault();
+      const path = e.target.getAttribute('data-href');
+      if (path) {
+        router.navigate(path);
+      }
+    }
+  });
+}
 
 export default styled(tpl, cs);
